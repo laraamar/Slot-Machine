2f1a11e2eba6a595e9cf6c184cbc31b7ea09b390
laraamar <amar_lara@bah.com>
2023-02-21 11:59:44 -0500

    Add Slot Machine files

---
+++ Slot-Machine/src/app/SlotMachine/SlotMachine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './SlotMachine.component.html',
+  styleUrls: ['./SlotMachine.component.css']
+})
+export class SlotMachineComponent {
+  title = 'Slot-Machine';
+}
