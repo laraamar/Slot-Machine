1ed3e7049b921704b2c441d3ddeb860e9493edfc
laraamar <amar_lara@bah.com>
2023-02-21 15:30:14 -0500

    Initialize main variables, created Spin function & Used a GetRandomInt function

--- Slot-Machine/src/app/SlotMachine/SlotMachine.component.ts
+++ Slot-Machine/src/app/SlotMachine/SlotMachine.component.ts
@@ -1,15 +1,66 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-My-SlotMachine',
   templateUrl: './SlotMachine.component.html',
   styleUrls: ['./SlotMachine.component.css']
 })
 export class SlotMachineComponent {
-  title = 'Slot-Machine';
-}
-Spin() {
+  public Symbols: String[] = [];
+  public credits: number = 10;
+  public isSpinning: boolean = false;
+  public firstSymbol: String = "";
+  public secondSymbol: String = "";
+  public thirdSymbol: String = "";
 
+  async Spin() {
+    if (this.credits <= 0) {
+      alert("Sorry you have no credits");
+      return;
+    }
+    this.credits --;
+    this.Symbols.push("C");
+    this.Symbols.push("L");
+    this.Symbols.push("O");
+    this.Symbols.push("W");
+
+    let firstSlot = this.GetRandomInt(0,1);
+    let secondSlot = this.GetRandomInt(0,1);
+    let thirdSlot = this.GetRandomInt(0,1);
+
+    this.isSpinning = true;
+    await this.delay(3000);
+    this.isSpinning = false;
+    this.firstSymbol = this.Symbols[firstSlot];
+    this.secondSymbol = this.Symbols[secondSlot];
+    this.thirdSymbol = this.Symbols[thirdSlot];
+
+    if (firstSlot === secondSlot && secondSlot === thirdSlot) {
+      let range= this.GetRandomInt(0,10);
+      if (this.credits >= 40 && this.credits <= 60) {
+        if (range<3){
+          console.log("Re-Rolled!");
+          this.Spin();
+          return;
+        }
+      }
+    else if (this.credits > 60) {
+      if (range<6) {
+        console.log("Re-Rolled!");
+          this.Spin();
+          return;
+      }
+    }
+    alert("Hit the jackpot!!!");
+    this.credits = this.credits + (firstSlot +1) *10;
+    }
+  }
+  GetRandomInt(min,max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max-min)+min);
+    //Max is exclusive and min is inclusive
+  }
 
 }
-}
+
